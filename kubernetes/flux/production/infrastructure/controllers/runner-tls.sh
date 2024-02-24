46d87637903f8efac231de60f0e9281db2ea3b7a
Kaejeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-24 11:55:04 +0000

    feat: gitlab runner

--- kubernetes/flux/production/infrastructure/controllers/runner-tls.sh
+++ kubernetes/flux/production/infrastructure/controllers/runner-tls.sh
@@ -4,4 +4,4 @@
 # Create a secret for the GitLab Runner to use to connect to the GitLab server
 # I used my intermediate CA certificate which I use to sign all my certificates in my srv-k8s.server.io domain
 INTERMEDIATE_CA_CERTIFICATE_PATH=
-k -n gitlab create secret generic gitlab-runner-tls --from-file=gitlab.srv-k8s.server.io.crt=${INTERMEDIATE_CA_CERTIFICATE_PATH}
+k -n gitlab-runner create secret generic gitlab-runner-certs --from-file=gitlab.server.io.crt=${INTERMEDIATE_CA_CERTIFICATE_PATH}
