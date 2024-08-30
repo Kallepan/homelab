29af84015174fddb1c50089479cb59abc4569a29
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-08-30 15:45:30 +0000

    chore: Update Rancher cluster names and annotations

--- terraform/rancher/main.tf
+++ terraform/rancher/main.tf
@@ -1,10 +1,23 @@
-resource "rancher2_cluster_v2" "cluster-staging" {
+
+
+resource "rancher2_cluster_v2" "cluster_staging" {
   name               = "staging"
   kubernetes_version = var.kubernetes_version
+
+  annotations = {
+    "ui.rancher/badge-color"      = "#00ff00"
+    "ui.rancher/badge-icon-text"  = "STG"
+    "field.cattle.io/description" = "Terraform managed cluster"
+  }
 }
 
-resource "rancher2_cluster_v2" "cluster-wasm" {
+resource "rancher2_cluster_v2" "cluster_wasm" {
   name               = "wasm"
   kubernetes_version = var.kubernetes_version
 
+  annotations = {
+    "ui.rancher/badge-color"      = "#0000ff"
+    "ui.rancher/badge-icon-text"  = "WSM"
+    "field.cattle.io/description" = "Terraform managed cluster"
+  }
 }
--- terraform/rancher/outputs.tf
+++ terraform/rancher/outputs.tf
@@ -1,3 +1,3 @@
 output "registration_token" {
-  value = nonsensitive(rancher2_cluster_v2.cluster-staging.cluster_registration_token)
+  value = nonsensitive(rancher2_cluster_v2.cluster_staging.cluster_registration_token)
 }
