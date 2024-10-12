ac0d2b73cd2de295e3b1a0abe4f7b2a562ddd9bc
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-10-12 09:00:12 +0000

    chore: remove mattermost s3 vars

--- terraform/cluster-prod/main.tf
+++ terraform/cluster-prod/main.tf
@@ -200,18 +200,6 @@ resource "kubernetes_secret" "gitlab-ca" {
 
 ### Mattermost ###
 
-resource "kubernetes_secret" "s3-creds-mattermost-fileStore" {
-  metadata {
-    name      = "mattermost-minio-creds"
-    namespace = "mattermost"
-  }
-
-  data = {
-    "accesskey" = var.mattermost_s3_access
-    "secretkey" = var.mattermost_s3_secret
-  }
-}
-
 resource "kubernetes_secret" "s3-creds-postgres-mattermost" {
   metadata {
     name      = "s3-creds"
--- terraform/cluster-prod/variables.tf
+++ terraform/cluster-prod/variables.tf
@@ -18,18 +18,6 @@ variable "backup_s3_secret" {
   sensitive   = true
 }
 
-variable "mattermost_s3_access" {
-  description = "The Access key to access Mattermost S3 bucket"
-  type        = string
-  sensitive   = true
-}
-
-variable "mattermost_s3_secret" {
-  description = "The Secret key to access Mattermost S3 bucket"
-  type        = string
-  sensitive   = true
-}
-
 variable "harbor_registry_s3_access" {
   description = "The Access key to access Harbor S3 bucket"
   type        = string
