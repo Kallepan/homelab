601abff3acc26a67759243e90e28f7263c3fa783
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-10-13 10:09:10 +0000

    feat: Add test cluster deployment
    Add a new GitLab project for deploying a test cluster. The project is named "Test Cluster" and has a description of "Test Cluster Deployment". It is set to private visibility and belongs to the "homelab_deployments" namespace.

--- terraform/gitlab/projects.tf
+++ terraform/gitlab/projects.tf
@@ -6,3 +6,11 @@ resource "gitlab_project" "homelab_iac_clustertemplate" {
   visibility_level = "private"
   namespace_id     = gitlab_group.homelab_iac.id
 }
+
+resource "gitlab_project" "homelab_deployments_test_cluster" {
+  path             = "test-cluster"
+  name             = "Test Cluster"
+  description      = "Test Cluster Deployment"
+  visibility_level = "private"
+  namespace_id     = gitlab_group.homelab_deployments.id
+}
