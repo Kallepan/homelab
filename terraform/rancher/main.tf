62689fd1de02da14a5f4f7803d522189a9d0d505
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-12-04 21:06:08 +0000

    fix: add production and staging clusters to Rancher configuration

--- terraform/rancher/main.tf
+++ terraform/rancher/main.tf
@@ -4,6 +4,23 @@ resource "rancher2_cluster" "core_cluster" {
   annotations = {
     "ui.rancher/badge-color" = "#001e57"
     "ui.rancher/badge-text"  = "CRE"
-    "ui.rancher/badge-text"  = "CRE"
+  }
+}
+
+resource "rancher2_cluster" "production_cluster" {
+  name        = "production"
+  description = "Production cluster"
+  annotations = {
+    "ui.rancher/badge-color" = "#001e57"
+    "ui.rancher/badge-text"  = "PRO"
+  }
+}
+
+resource "rancher2_cluster" "staging_cluster" {
+  name        = "staging"
+  description = "Staging cluster"
+  annotations = {
+    "ui.rancher/badge-color" = "#001e57"
+    "ui.rancher/badge-text"  = "STG"
   }
 }
